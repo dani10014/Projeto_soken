35628744222cdd4b711c3cbb477adf9a6cf18dc8
Daniel <dijalmaduartefleitas0@gmail.com>
2025-10-26 12:01:41 -0400

    Adiconando popup nas imagens abaixo

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 $(document).ready(function() {
 
-    $(".imagem-central").slick({
+    var $carousel = $(".imagem-central");
+
+    $carousel.slick({
         autoplay:true,
+        slidetoshow:1,
     });
+
     $(".menu").on("click",function(){
         $(".menu-lateral").toggleClass("ativo");
     })
     $(".botao-fechar").on("click",function(){
         $(".menu-lateral").removeClass("ativo");
     })
+    
+    $carousel.magnificPopup({
+        delegate: 'a:not(.slick-cloned)', // Delega o clique para os links que NÃO são clones do Slick
+        type: 'image',
+        gallery:{
+            enabled:true
+        }
+    });
 })
