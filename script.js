4de5d8c56da0f69001f0d3ad4e3828f3a04d6a03
Daniel <dijalmaduartefleitas0@gmail.com>
2025-10-27 22:51:10 -0400

    Adicionando seçao sobre

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
 
+    $(document).on("click", function(event) {
+        const $target = $(event.target);
+        if (!$target.closest('.menu-lateral').length && !$target.closest('.menu').length) {
+            $('.menu-lateral').removeClass('ativo');
+        }
+    });
+
     var $carousel = $(".imagem-central");
 
     $carousel.slick({
@@ -7,9 +14,11 @@ $(document).ready(function() {
         slidetoshow:1,
     });
 
-    $(".menu").on("click",function(){
+    
+    $(".menu").on("click",function(e){
         $(".menu-lateral").toggleClass("ativo");
-    })
+        e.stopPropagation();
+    });
     $(".botao-fechar").on("click",function(){
         $(".menu-lateral").removeClass("ativo");
     })
@@ -21,7 +30,8 @@ $(document).ready(function() {
             enabled:true
         }
     });
-    $(".menu-lateral #contato").on("click", function(event) {
+
+    $(".menu-lateral .secao-links a").on("click", function(event) {
         $(".menu-lateral").removeClass("ativo");
         event.preventDefault();
         
@@ -30,41 +40,4 @@ $(document).ready(function() {
             scrollTop: caminho.offset().top
         }, 800); 
     });
-});
-    $(".menu-lateral #sobre").on("click", function(event) {
-        $(".menu-lateral").removeClass("ativo");
-        event.preventDefault(); 
-    
-        let caminho = $("#container-about");
-        $('html, body').animate({
-            scrollTop: caminho.offset().top
-        }, 800); 
-    });
-    $(".menu-lateral #social").on("click", function(event) {
-        $(".menu-lateral").removeClass("ativo");
-        event.preventDefault();
-        
-        let caminho = $("#container-about");
-        $('html, body').animate({
-            scrollTop: caminho.offset().top
-        }, 800); 
-    });
-    $(".menu-lateral #endereco").on("click", function(event) {
-        $(".menu-lateral").removeClass("ativo");
-        event.preventDefault(); 
-
-        let caminho = $("#container-about");
-        $('html, body').animate({
-            scrollTop: caminho.offset().top
-        }, 800); 
-    });
-    $(".menu-lateral #termos-legais").on("click", function(event) {
-        $(".menu-lateral").removeClass("ativo");
-        event.preventDefault(); 
-
-        let caminho = $("#container-about");
-        $('html, body').animate({
-            scrollTop: caminho.offset().top
-        }, 800); 
-    });
-
+})
